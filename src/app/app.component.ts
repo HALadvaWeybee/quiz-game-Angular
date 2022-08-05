5ed96b31add056f6ed8944da6f11441afa438787
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-08-05 12:24:36 +0530

    complete the quiz app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,85 +3,79 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'quizTask';
+  question: any;
+  a: any;
+  b: any;
+  c: any;
+  d: any;
+  menuShow: boolean = true;
+  resultShow: boolean = false;
 
-  mcqData:any = [
-    {
-      question: "Which of the following tag is used for drop down list?",
-      a: "select",
-      b: "text",
-      c: "textarea",
-      d: "dropdown",
-      answer: "a",
-    },
-    {
-      question: "By which tag, an unordered list is represented?",
-      a: "u",
-      b: "I",
-      c: "ul",
-      d: "ol",
-      answer: "c",
-    },
-    {
-      question: "What is HTML",
-      a: "markup language",
-      b: "programming language",
-      c: "not a language",
-      d: "none of these",
-      answer: "a",
-    },
-    // {
-    //   question: "What is the default item marker in unordered lists of HTML?",
-    //   a: "Circle",
-    //   b: "Marker",
-    //   c: "disc",
-    //   d: "None of the above",
-    //   answer: "c",
-    // },
-    // {
-    //   question: " Which one of the following is a form element?",
-    //   a: "text box",
-    //   b: "radio button",
-    //   c: "submit button",
-    //   d: "All of these",
-    //   answer: "d",
-    // },
-    // {
-    //   question: "Which one of the following is incorrect?",
-    //   a: "<label> tag in HTML is used for creating a tag for form elements.",
-    //   b: "<label> can be used to increase the clickable area of buttons",
-    //   c: "id attribute is used with <label> to increase the clickable area of form elements",
-    //   d: "None of the above",
-    //   answer: "c",
-    // },
-  ];
-  question:any = "Which one of the following is a form element?";
-  a=  "text box"
-  b=  "radio button"
-  c= "submit button"
-  d= "All of these"
-  i = 0;
-  j:any = 0;
-
-  answers:string[] = ["select","ul","markup language","All of these"];
-  receiveAns:string[]=[];
-  tempData:any[] = this.mcqData;
-  select(item:string) {
-     console.log(item); 
-     this.receiveAns.push(item);
-     for(this.j = 0; this.j < this.tempData.length; this.j++) {
-         this.question = this.tempData[this.j].question;
-         this.a = this.tempData[this.j].a;
-         this.b = this.tempData[this.j].b;
-         this.c = this.tempData[this.j].c;
-         this.d = this.tempData[this.j].d;      
-         this.tempData.splice(this.j, 1);
-         break;
-     }    
-     console.log(this.receiveAns);  
+  constructor() {
+    this.question = 'Which of the following tag is used for drop down list?';
+    this.a = 'select';
+    this.c = 'textarea';
+    this.b = 'text';
+    this.d = 'dropdown';
   }
+  mcqData: any = [
+    {
+      question: 'Which of the following tag is used for drop down list?',
+      a: 'select',
+      b: 'text',
+      c: 'textarea',
+      d: 'dropdown',
+      answer: 'a',
+    },
+    {
+      question: 'By which tag, an unordered list is represented?',
+      a: 'u',
+      b: 'I',
+      c: 'ul',
+      d: 'ol',
+      answer: 'c',
+    },
+    {
+      question: 'What is HTML',
+      a: 'markup language',
+      b: 'programming language',
+      c: 'not a language',
+      d: 'none of these',
+      answer: 'a',
+    },
+    {
+      question: ' Which one of the following is a form element?',
+      a: 'text box',
+      b: 'radio button',
+      c: 'submit button',
+      d: 'All of these',
+      answer: 'd',
+    },
+  ];
+  answers: any[] = this.mcqData.map((ele: any) => ele.answer);
+  tempArr: any[] = this.mcqData;
+  count: number = 0;
   
+  select(item: any) {
+    if (this.tempArr.length === 1) {
+      this.menuShow = false;
+      this.resultShow = true;
+    }
+    if (this.answers[0] == item) {
+      this.count = this.count + 1;
+    }
+    if (this.tempArr.length > 1) {
+      this.tempArr.splice(0, 1);
+      this.answers.splice(0, 1);
+      this.question = this.tempArr[0].question;
+      this.a = this.tempArr[0].a;
+      this.b = this.tempArr[0].b;
+      this.c = this.tempArr[0].c;
+      this.d = this.tempArr[0].d;
+    }
+  }
 }
